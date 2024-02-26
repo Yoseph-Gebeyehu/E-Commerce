21f17e0310debbe2794efc5b6fb6e3eb73d93bab
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2024-02-26 13:44:36 +0300

    Add firebase for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:e_commerce/sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -31,40 +36,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return const MaterialApp(
+      home: SignInPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
---
+++ lib/sign_in.dart
@@ -0,0 +1,255 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  TextEditingController userNameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  bool showPassword = true;
+  bool isSignIn = true;
+  @override
+  Widget build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: deviceSize.height * 0.4,
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(200.0),
+                    ),
+                  ),
+                  child: Image.asset(
+                    'assets/mac_image.webp',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 5,
+                  right: deviceSize.width * 0.5,
+                  left: deviceSize.width * 0.2,
+                  top: deviceSize.height * 0.35,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: isSignIn ? Colors.green : Colors.white,
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(10.0),
+                        topLeft: Radius.circular(10.0),
+                      ),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          isSignIn = true;
+                        });
+                      },
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(
+                          color: isSignIn ? Colors.white : Colors.green,
+                          fontSize: deviceSize.width * 0.036,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 5,
+                  right: deviceSize.width * 0.2,
+                  left: deviceSize.width * 0.5,
+                  top: deviceSize.height * 0.35,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: isSignIn ? Colors.white : Colors.green,
+                      borderRadius: const BorderRadius.only(
+                        bottomRight: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
+                      ),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          isSignIn = false;
+                        });
+                      },
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(
+                          color: isSignIn ? Colors.green : Colors.white,
+                          fontSize: deviceSize.width * 0.036,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: deviceSize.height * 0.03),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: deviceSize.width * 0.05),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1,
+                ),
+              ),
+              height: isSignIn
+                  ? deviceSize.height * 0.17
+                  : deviceSize.height * 0.25,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ListTile(
+                    leading: const Icon(
+                      Icons.person,
+                      color: Colors.green,
+                    ),
+                    title: TextFormField(
+                      controller: userNameController,
+                      cursorColor: Colors.green,
+                      decoration: const InputDecoration(
+                        hintText: 'User Name',
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                  const Divider(),
+                  Visibility(
+                    visible: !isSignIn,
+                    child: ListTile(
+                      leading: const Icon(
+                        Icons.email,
+                        color: Colors.green,
+                      ),
+                      title: TextFormField(
+                        controller: emailController,
+                        cursorColor: Colors.green,
+                        decoration: const InputDecoration(
+                          hintText: 'Email',
+                          border: InputBorder.none,
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Visibility(
+                    visible: !isSignIn,
+                    child: const Divider(),
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.lock,
+                      color: Colors.green,
+                    ),
+                    title: TextFormField(
+                      controller: passwordController,
+                      cursorColor: Colors.green,
+                      obscureText: showPassword,
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    trailing: InkWell(
+                      onTap: () {
+                        setState(() {
+                          showPassword = !showPassword;
+                        });
+                      },
+                      child: Icon(
+                        showPassword ? Icons.visibility_off : Icons.visibility,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: deviceSize.height * 0.03),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                minimumSize: Size(
+                  deviceSize.width * 0.8,
+                  deviceSize.height * 0.06,
+                ),
+              ),
+              child: Text(
+                isSignIn ? 'Sign in' : 'Sign up',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: deviceSize.width * 0.04,
+                ),
+              ),
+            ),
+            SizedBox(height: deviceSize.height * 0.05),
+            Text(
+              isSignIn ? 'Or Sign in with' : 'Or Sign up with',
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: deviceSize.width * 0.04,
+              ),
+            ),
+            SizedBox(height: deviceSize.height * 0.01),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  FontAwesomeIcons.facebook,
+                  color: Colors.green,
+                ),
+                Icon(
+                  FontAwesomeIcons.twitter,
+                  color: Colors.green,
+                ),
+                Icon(
+                  FontAwesomeIcons.google,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            SizedBox(height: deviceSize.height * 0.01),
+            Divider(
+              endIndent: deviceSize.width * 0.05,
+              indent: deviceSize.width * 0.05,
+            ),
+            SizedBox(height: deviceSize.height * 0.04),
+            Visibility(
+              visible: isSignIn,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: deviceSize.width * 0.04,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
