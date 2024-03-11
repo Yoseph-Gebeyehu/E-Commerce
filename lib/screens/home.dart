c96b25c9de179588910d02620036e1ad6400919a
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2024-03-11 15:46:32 +0300

    Add tabbar on HomePage

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -40,6 +40,13 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  List<Product> filterCategory(String category) {
+    return productList!.products
+        .where((product) =>
+            product.category.toLowerCase() == category.toLowerCase())
+        .toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size deviceSize = MediaQuery.of(context).size;
@@ -51,137 +58,166 @@ class _HomePageState extends State<HomePage> {
         ),
       );
     } else {
-      return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          elevation: 10,
-          title: const Text('Product Lists'),
-          actions: [
-            InkWell(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      backgroundColor: Colors.white,
-                      title: const Text(
-                        'Log out of your account?',
-                        textAlign: TextAlign.center,
-                      ),
-                      content: SizedBox(
-                        height: deviceSize.height * 0.17,
-                        child: Column(
-                          children: [
-                            const SizedBox(height: 15),
-                            const Divider(),
-                            TextButton(
-                              onPressed: () {
-                                signUserOut();
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => Decider(),
+      return DefaultTabController(
+        length: 7,
+        child: Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            elevation: 10,
+            title: const Text('Product Lists'),
+            actions: [
+              InkWell(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        backgroundColor: Colors.white,
+                        title: const Text(
+                          'Log out of your account?',
+                          textAlign: TextAlign.center,
+                        ),
+                        content: SizedBox(
+                          height: deviceSize.height * 0.17,
+                          child: Column(
+                            children: [
+                              const SizedBox(height: 15),
+                              const Divider(),
+                              TextButton(
+                                onPressed: () {
+                                  signUserOut();
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Decider(),
+                                    ),
+                                  );
+                                },
+                                child: Text(
+                                  'Logout',
+                                  style: TextStyle(
+                                    fontSize: deviceSize.width * 0.05,
+                                    color: Colors.red,
                                   ),
-                                );
-                              },
-                              child: Text(
-                                'Logout',
-                                style: TextStyle(
-                                  fontSize: deviceSize.width * 0.05,
-                                  color: Colors.red,
                                 ),
                               ),
-                            ),
-                            const Divider(),
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text(
-                                'Cancel',
-                                style: TextStyle(
-                                  fontSize: deviceSize.width * 0.05,
+                              const Divider(),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  'Cancel',
+                                  style: TextStyle(
+                                    fontSize: deviceSize.width * 0.05,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      contentPadding: const EdgeInsets.all(0),
-                    );
-                  },
-                );
-              },
-              child: const Padding(
-                padding: EdgeInsets.only(right: 15.0),
-                child: Icon(Icons.logout),
-              ),
-            ),
-          ],
-        ),
-        body: Center(
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: deviceSize.width * 0.0375,
-              crossAxisSpacing: deviceSize.width * 0.0375,
-              childAspectRatio: 1 / 1,
-            ),
-            itemBuilder: (context, i) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 2),
-                child: GridTile(
-                  footer: Container(
-                    color: Colors.black.withOpacity(0.8),
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 8.0,
-                      horizontal: 16.0,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "${productList!.products[i].price} br",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: deviceSize.width * 0.036,
+                            ],
                           ),
                         ),
-                        Flexible(
-                          child: Text(
-                            productList!.products[i].category,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ProductDetailPage(
-                          product: productList!.products[i],
-                        ),
-                      ));
+                        contentPadding: const EdgeInsets.all(0),
+                      );
                     },
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(24),
-                      child: Image.network(
-                        productList!.products[i].thumbnail,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
+                  );
+                },
+                child: const Padding(
+                  padding: EdgeInsets.only(right: 15.0),
+                  child: Icon(Icons.logout),
                 ),
-              );
-            },
-            itemCount: productList!.products.length,
+              ),
+            ],
+            bottom: const TabBar(
+              isScrollable: true,
+              tabs: [
+                Text('All'),
+                Text('Smartphones'),
+                Text('Laptops'),
+                Text('Fragrances'),
+                Text('Skincare'),
+                Text('Groceries'),
+                Text('Home Decoration'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              gridView(deviceSize, productList!.products),
+              gridView(deviceSize, filterCategory('smartphones')),
+              gridView(deviceSize, filterCategory('laptops')),
+              gridView(deviceSize, filterCategory('fragrances')),
+              gridView(deviceSize, filterCategory('skincare')),
+              gridView(deviceSize, filterCategory('groceries')),
+              gridView(deviceSize, filterCategory('home-decoration')),
+            ],
           ),
         ),
       );
     }
+  }
+
+  Center gridView(Size deviceSize, List<Product> products) {
+    return Center(
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: deviceSize.width * 0.0375,
+          crossAxisSpacing: deviceSize.width * 0.0375,
+          childAspectRatio: 1 / 1,
+        ),
+        itemBuilder: (context, i) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2),
+            child: GridTile(
+              footer: Container(
+                color: Colors.black.withOpacity(0.8),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 16.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "${products[i].price} br",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: deviceSize.width * 0.036,
+                      ),
+                    ),
+                    Flexible(
+                      child: Text(
+                        products[i].category,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ProductDetailPage(
+                      product: products[i],
+                    ),
+                  ));
+                },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(24),
+                  child: Image.network(
+                    products[i].thumbnail,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: products.length,
+      ),
+    );
   }
 }
